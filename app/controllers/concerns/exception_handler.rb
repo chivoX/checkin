70b0ea1a302ccc51f8a5fe9730b3c0797d767854
Ivan Granados <chivox.ivan@gmail.com>
2019-11-14 21:39:52 -0500

    master.key saved just for this test, you should never include this file into your source control

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -10,10 +10,10 @@ module ExceptionHandler
 
   included do
     # Define custom handlers
-    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
-    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
-    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::MissingToken, with: :unprocessable_entity
+    rescue_from ExceptionHandler::InvalidToken, with: :unprocessable_entity
 
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
@@ -23,7 +23,7 @@ module ExceptionHandler
   private
 
   # JSON response with message; Status code 422 - unprocessable entity
-  def four_twenty_two(error)
+  def unprocessable_entity(error)
     json_response({ message: error.message }, :unprocessable_entity)
   end
 
